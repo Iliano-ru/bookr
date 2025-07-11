09772ca47d458f7a2fb37d0ed8b83f585e8feb7b
Ilia <n_i_d@bk.ru>
2025-07-11 11:17:39 +0400

    chapter-4-end

--- bookr/reviews/admin.py
+++ bookr/reviews/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import *
+
+
+class BookAdmin(admin.ModelAdmin):
+    @admin.display(
+        ordering='isbn',
+        description='ISBN-13',
+        empty_value='-/-'
+    )
+    def isbn13(self):
+        """ '9780316769174' => '978-0-31-676917-4' """
+        return "{}-{}-{}-{}-{}".format(self.isbn[0:3],
+                                       self.isbn[3:4], self.isbn[4:6],
+                                       self.isbn[6:12], self.isbn[12:13])
+
+    date_hierarchy = 'publication_date'
+    list_display = ('title', isbn13)
+    list_filter = ('publisher', 'publication_date')
+    search_fields = ('title', 'isbn')
+
 
 # Register your models here.
+admin.site.register(Publisher)
+admin.site.register(Contributor)
+admin.site.register(Book, BookAdmin)
+admin.site.register(BookContributor)
+admin.site.register(Review)
